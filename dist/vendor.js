fb67a149bd8d6db3577f7e43e62c47a392472ef3
Eugene Chechurin <e.chechurin@gmail.com>
2016-04-12 11:41:58 -0700

    build

--- dist/vendor.js
+++ dist/vendor.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "82b33b3d99fae659684d"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "597321cb131064f69413"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
